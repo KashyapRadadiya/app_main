43024e8fd65d424d6bbcfebf6253844e4d11741d
Kashyap Radadiya <130830541+KashyapRadadiya@users.noreply.github.com>
2025-07-10 11:20:48 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Basic PHP kashyap</title>
+</head>
+<body>
+
+<h1>Welcome to My PHP Page</h1>
+
+<?php
+echo "<p>Hello from KASHYAP!</p>";
+echo "<p>Today is " . date("l, F jS Y") . ".</p>";
+?>
+
+</body>
+</html>
